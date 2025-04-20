b86893603e4e50667bf5515ece9b200ac09360e8
Lydia Martín <lydiamartin@MacBook-Air-de-Lydia.local>
2025-04-20 13:36:21 +0200

    api listra

--- db.js
+++ db.js
@@ -1,53 +1,110 @@
-import dotenv from "dotenv";
-dotenv.config();//lee el fichero .env y crea las variables de entorno
-//---------------------------
 import postgres from "postgres";
 
 function conectar(){
     return postgres({
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD
+        host : process.env.DB_HOST,
+        database : process.env.DB_NAME,
+        user : process.env.DB_USER,
+        password : process.env.DB_PASSWORD 
     });
 }
 
 export function leerTareas(){
-    return new Promise(async (ok, ko) => {
+    return new Promise(async (ok,ko) => {
         let conexion = conectar();
+
         try{
-            let tareas = await conexion`SELECT * FROM tareas`;
+
+            let tareas = await conexion`SELECT * FROM tareas ORDER BY id`;
+
             conexion.end();
+
             ok(tareas);
+
         }catch(error){
-            ko({error: "error bbdd"});
+
+            ko({ error : "error bbdd" });
+
         }
     });
 }
 
 export function crearTarea(tarea){
-    return new Promise(async (ok, ko) => {
+    return new Promise(async (ok,ko) => {
         let conexion = conectar();
+
         try{
+
             let [{id}] = await conexion`INSERT INTO tareas (tarea) VALUES (${tarea}) RETURNING id`;
+
             conexion.end();
+
             ok(id);
+
         }catch(error){
-            ko({error: "error bbdd"});
+
+            ko({ error : "error bbdd" });
+
         }
     });
 }
 
 export function borrarTarea(id){
-    return new Promise(async (ok, ko) => {
+    return new Promise(async (ok,ko) => {
         let conexion = conectar();
+
         try{
+
             let {count} = await conexion`DELETE FROM tareas WHERE id = ${id}`;
+
             conexion.end();
-            ok(id);
+
+            ok(count);
+
         }catch(error){
-            ko({error: "error bbdd"});
+            
+            ko({ error : "error bbdd" });
+
         }
     });
 }
 
+export function editarTarea(id,texto){
+    return new Promise(async (ok,ko) => {
+        let conexion = conectar();
+
+        try{
+
+            let {count} = await conexion`UPDATE tareas SET tarea = ${texto} WHERE id = ${id}`;
+
+            conexion.end();
+
+            ok(count);
+
+        }catch(error){
+            
+            ko({ error : "error bbdd" });
+
+        }
+    });
+}
+
+export function editarEstado(id){
+    return new Promise(async (ok,ko) => {
+        let conexion = conectar();
+
+        try{
+
+            let {count} = await conexion`UPDATE tareas SET estado = NOT estado WHERE id = ${id}`;
+
+            conexion.end();
+
+            ok(count);
+
+        }catch(error){
+            
+            ko({ error : "error bbdd" });
+
+        }
+    });
+}
--- index.js
+++ index.js
@@ -1,10 +1,106 @@
 import dotenv from "dotenv";
 dotenv.config();//lee el fichero .env y crea las variables de entorno
-//---------------------------
+//-------------
 import express from "express";
-
-
+import { leerTareas,crearTarea,borrarTarea,editarTarea,editarEstado } from "./db.js";
 
 const servidor = express();
+
+
+servidor.use(express.json());
+
+if(process.env.PRUEBAS){
+    servidor.use(express.static("./pruebas"));
+}
+
+servidor.get("/tareas", async (peticion,respuesta) => {
+    try{
+        let tareas = await leerTareas();
+
+        respuesta.json(tareas);
+
+    }catch(error){
+
+        respuesta.status(500);
+
+        respuesta.json({ error : "error en el servidor" });
+
+    }
+});
+
+servidor.post("/tareas/nueva", async (peticion,respuesta,siguiente) => {
+    
+    let {tarea} = peticion.body;
+
+    if(!tarea || tarea.trim() == ""){
+        return siguiente(true);
+    }
+
+    try{
+
+        let id = await crearTarea(tarea);
+
+        respuesta.json({id});
+
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json({ error : "error en el servidor" });
+    }
+    
+});
+
+servidor.delete("/tareas/borrar/:id([0-9]+)", async (peticion,respuesta) => {
+    try{
+
+        let id = Number(peticion.params.id);
+
+        let cantidad = await borrarTarea(id);
+
+        respuesta.json({ resultado : cantidad ? "ok" : "ko" });
+
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json({ error : "error en el servidor" });
+    }
+});
+
+servidor.put("/tareas/actualizar/:id([0-9]+)/:operacion(1|2)", async (peticion,respuesta,siguiente) => {
+    let operacion = Number(peticion.params.operacion);
+    let id = Number(peticion.params.id);
+    let {tarea} = peticion.body;
+
+    let operaciones = [editarTarea,editarEstado];
+
+    if(operacion == 1 && (!tarea || tarea.trim() == "")){
+        return siguiente(true);
+    }
+
+    try{
+
+        let cantidad = await operaciones[operacion - 1](id,tarea);//clever code
+
+        respuesta.json({ resultado : cantidad ? "ok" : "ko" });
+
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json({ error : "error en el servidor" });
+    }
+    
+});
+
+
+servidor.use((error,peticion,respuesta,siguiente) => {
+    respuesta.status(400);
+    respuesta.json({ error : "error en la petición" });
+});
+
+servidor.use((peticion,respuesta) => {
+    respuesta.status(404);
+    respuesta.json({ error : "recurso no encontrado" });
+});
+
 
 servidor.listen(process.env.PORT);
